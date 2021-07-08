4ecc82e8db622d4bf5ed670b0de17669099303ea
James <james.ttan64@gmail.com>
2021-07-08 18:58:41 -0400

    Added a new Converter class

--- stuff/Converters.py
+++ stuff/Converters.py
@@ -2,6 +2,8 @@ import discord
 import asyncio
 from discord.ext import commands
 from Main import get_client
+from stuff.CustomExceptions import ArgumentError
+
 
 class custom_emoji_converter(commands.Converter):
     async def convert(self, ctx, argument):
@@ -17,4 +19,18 @@ class custom_emoji_converter(commands.Converter):
                 emoji = argument
 
         return emoji
+
+class dur_secs_converter(commands.Converter):
+    async def convert (self, ctx, argument:str):
+        time = argument.split(":")
+        dur_secs = 0
         
+        if not len(time) == 4 or not argument.replace(":","").isdigit():
+            dur_secs = None
+            raise ArgumentError("Incorrect input and/or format for the duration field")
+
+        else:
+            dur_secs += int(time[0]) * 86400 + int(time[1]) * 3600 + int(time[2]) * 60 + int(time[3])
+        
+        return dur_secs
+
