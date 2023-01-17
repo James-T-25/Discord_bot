1846ce19dd78df5302d4cea6c382501ee0efc7f6
james_t <james.ttan64@gmail.com>
2023-01-17 17:55:39 -0500

    start of rewrite

--- Main.py
+++ Main.py
@@ -1,83 +1,28 @@
 import os
 import discord
 from discord.ext import commands
-from stuff.botdomain import World
 from dotenv import load_dotenv
 
 load_dotenv()
-intents = discord.Intents().default()
-intents.members = True
 
-client = commands.AutoShardedBot(
-    command_prefix= ["k."],
-    case_insensitive = True,
-    intents = intents
-)
+intents = discord.Intents.default()
+intents.message_content = True
 
-client.remove_command("help")
+class MyClient(discord.Client):
+    def __init__(self, *, intents, **options) -> None:
+        super().__init__(intents=intents, **options)
+        self.tree = discord.app_commands.CommandTree(self)
 
-world = World()
+    async def on_ready(self):
+        await self.tree.sync(guild=discord.Object(id=790714282551017512))
+        print('Logged on as', self.user)
 
-def get_world() -> World:
-    return world
+intents = discord.Intents.default()
+intents.message_content = True
+myclient = MyClient(intents=intents)
 
-def get_client() -> discord.Client:
-    return client
+@myclient.tree.command(name="ping", guild = discord.Object(id= 790714282551017512))
+async def ping(interaction:discord.Interaction):
+    await interaction.response.send_message("Pong!", ephemeral=True)
 
-@client.event
-async def on_ready():
-    for home in client.guilds:
-        world.add_guild(home.id)
-    print('Bot is ready.')
-
-@client.command(hidden = True)
-@commands.is_owner()
-async def load(ctx, extension):
-    try:
-        client.load_extension(f'cogs.{extension}')
-        print(f'{extension} has been loaded')
-    except Exception as err:
-        print(err)
-        raise err
-
-@client.command(hidden = True)
-@commands.is_owner()
-async def unload(ctx, extension):
-    try:
-        client.unload_extension(f'cogs.{extension}')
-        print(f'{extension} has been unloaded')
-    except Exception as err:
-        print(err)
-        raise err
-
-@client.command(hidden=True)
-@commands.is_owner()
-async def reload(ctx, extension):
-    try: 
-        client.unload_extension(f'cogs.{extension}')
-        client.load_extension(f'cogs.{extension}')
-        print(f'{extension} has been reloaded')
-
-    except Exception as err:
-        print(err)
-        raise err
-
-@client.command(hidden=True)
-@commands.is_owner()
-async def end(ctx):
-    embed = discord.Embed(title = "Farewell", colour = discord.Colour.red())
-    embed.set_image(url= "https://media.giphy.com/media/sJ7EDbXwEIygU/giphy.gif")
-    await ctx.send(embed = embed)
-    await ctx.bot.logout()
-
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
-
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency*1000)} ms')
-
-
-client.run(os.getenv("token"))
+myclient.run(os.getenv('bot_token'))
--- stuff/CustomExceptions.py
+++
@@ -1,5 +0,0 @@
-import discord
-from discord.ext import commands
-class ArgumentError(commands.MissingRequiredArgument):
-    def __init__(self, message):
-        self.message = message
